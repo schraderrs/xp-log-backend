68eb5390c3cbb06d1e9e5de57e4a6a52b689a9bf
schraderrs <45096112+schraderrs@users.noreply.github.com>
2019-11-07 19:31:44 +0100

    Base template & first contact with database
    .env - configured the link to the database
    2019_11_07_133805_entries - first made entry for the database

---
+++ xp-log-backend/database/migrations/2019_11_07_133805_entries.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Entries extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('entries', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('badge');
+            $table->string('media');
+            $table->integer('student_id');
+            $table->text('text');
+            $table->enum('status',['prive', 'public', 'uploaded']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('entries');
+    }
+}
