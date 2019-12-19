25ea9c6569520a09d2ca92d3aa623ad425362cc4
schraderrs <45096112+schraderrs@users.noreply.github.com>
2019-12-19 14:57:29 +0100

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,7 @@ use Illuminate\Http\Request;
 // Route::middleware('auth:api')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
-use App/Student;
+use App\Student;
 
 Route::get('/test/{id}', function(Student $students){
   return $students;
